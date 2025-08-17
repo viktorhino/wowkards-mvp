2558c0a731fb5f5876e0ed162656aec79c52174c
Viktor Lozano <73148085+viktorhino@users.noreply.github.com>
2025-08-17 13:34:56 -0500

    build: alias @ y casing para Vercel

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+import path from "path";
+const nextConfig = {
+  webpack: (config: any) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
+
 export default nextConfig;
